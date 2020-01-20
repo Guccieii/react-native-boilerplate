6874e7d3ac4f06139c8bed34d59aca08db768a12
Vincenzo Guccione <v.guccione@6mobile.mobi>
2020-01-20 18:25:44 +0100

    add SafeAreaView

--- src/views/Splash/index.js
+++ src/views/Splash/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 class Splash extends Component {
 	render() {
 		return (
-			<View>
+			<SafeAreaView style={{backgroundColor: 'red'}}>
 				<Text>Welcome</Text>
-			</View>
+			</SafeAreaView>
 		);
 	}
 }
