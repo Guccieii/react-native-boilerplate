8f8f8a252199fe74f17d12fa76022a2831932571
Vincenzo Guccione <v.guccione@6mobile.mobi>
2020-07-03 15:52:51 +0200

    Update package version

--- src/components/CustomInput/index.js
+++ src/components/CustomInput/index.js
@@ -2,16 +2,53 @@ import React, { Component } from 'react';
 import { View, Text, TextInput } from 'react-native';
 
 class CustomInput extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { error: true };
+	}
 	render() {
-		const { name, placeholder } = this.props;
+		const { label, placeholder, type } = this.props;
+		const { error } = this.state;
 
 		return (
-			<View style={{ backgroundColor: 'yellow' }}>
-				<Text>{name}</Text>
-				<TextInput placeholder={placeholder} />
-				<Text>Errore!</Text>
+			<View style={{ backgroundColor: 'white', marginHorizontal: 16, marginTop: 8 }}>
+				<Text style={{ fontSize: 16, paddingHorizontal: 8 }}>{label}</Text>
+				<TextInput
+					placeholder={placeholder}
+					style={{
+						borderWidth: 2,
+						borderRadius: 8,
+						fontSize: 16,
+						paddingHorizontal: 8,
+						borderColor: error ? 'orangered' : 'black'
+					}}
+					secureTextEntry={type == 'password' ? true : false}
+					onChangeText={(text) => this.handleChangeText(text, type)}
+					keyboardType={this.setKeyboardType(type)}
+				/>
 			</View>
 		);
+	}
+
+	//Show the appropriate keyboard according to the type
+	setKeyboardType(type) {
+		switch (type) {
+			case 'email':
+				return 'email-address';
+			case 'phone':
+				return 'phone-pad';
+			case 'numeric':
+				return 'numeric';
+			default:
+				return 'default';
+		}
+	}
+
+	handleChangeText(text, type) {
+		if (text == '') {
+			this.setState({ error: true });
+		}
 	}
 }
 
--- src/views/Splash/index.js
+++ src/views/Splash/index.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import CustomInput from './../../components/CustomInput';
-import { Translate } from "react-localize-redux";
+import { Translate } from 'react-localize-redux';
 
 class Splash extends Component {
 	render() {
 		return (
-			<View style={{ backgroundColor: 'red' }}>
-				<Text><Translate id="welcome" /></Text>
-				<CustomInput name='email' placeholder='inserisci email' />
+			<View style={{ backgroundColor: 'white', flex: 1 }}>
+				<Text>
+					<Translate id="welcome" />
+				</Text>
+				<CustomInput label="email" type="email" placeholder="inserisci email" />
+				<CustomInput label="password" type="password" placeholder="inserisci password" />
 			</View>
 		);
 	}
