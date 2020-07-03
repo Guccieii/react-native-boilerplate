77c61b59d4ce6c90d61b4e5b9d4969472a3c64e2
Vincenzo Guccione <v.guccione@6mobile.mobi>
2020-07-03 18:50:42 +0200

    Update customInput and customButton

--- src/components/CustomInput/index.js
+++ src/components/CustomInput/index.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput } from 'react-native';
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+
 class CustomInput extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { error: true };
+		this.state = { error: true, value: '' };
 	}
 	render() {
-		const { label, placeholder, type } = this.props;
+		const { label, placeholder, type, showLabel = true } = this.props;
 		const { error } = this.state;
 
 		return (
 			<View style={{ backgroundColor: 'white', marginHorizontal: 16, marginTop: 8 }}>
-				<Text style={{ fontSize: 16, paddingHorizontal: 8 }}>{label}</Text>
+				<Text style={{ fontSize: 16, paddingHorizontal: 8 }}>{showLabel ? label : ''}</Text>
 				<TextInput
 					placeholder={placeholder}
 					style={{
-						borderWidth: 2,
-						borderRadius: 8,
+						borderBottomWidth: 2,
 						fontSize: 16,
 						paddingHorizontal: 8,
 						borderColor: error ? 'orangered' : 'black'
@@ -46,8 +48,43 @@ class CustomInput extends Component {
 	}
 
 	handleChangeText(text, type) {
+		const { onChangeField, label } = this.props;
+		const { error } = this.state;
+
 		if (text == '') {
-			this.setState({ error: true });
+			this.setState({ error: true, value: text }, () => {
+				onChangeField(text, label, error);
+			});
+		} else {
+			switch (type) {
+				case 'email':
+					if (emailRegex.test(text)) {
+						this.setState({ error: false, value: text }, () => {
+							onChangeField(text, label, error);
+						});
+					} else {
+						this.setState({ error: true, value: text }, () => {
+							onChangeField(text, label, error);
+						});
+					}
+					break;
+				case 'phone':
+					if (phoneRegex.test(text)) {
+						this.setState({ error: false, value: text }, () => {
+							onChangeField(text, label, error);
+						});
+					} else {
+						this.setState({ error: true, value: text }, () => {
+							onChangeField(text, label, error);
+						});
+					}
+					break;
+				default:
+					this.setState({ error: false, value: text }, () => {
+						onChangeField(text, label, error);
+					});
+					break;
+			}
 		}
 	}
 }
--- src/views/Splash/index.js
+++ src/views/Splash/index.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import CustomInput from './../../components/CustomInput';
+import CustomButton from './../../components/CustomButton';
 import { Translate } from 'react-localize-redux';
+import { ScrollView } from 'react-native-gesture-handler';
 
 class Splash extends Component {
+	onChangeField = (text, label, error) => {
+		console.log(text, label, error);
+	};
+
+	onPress() {
+		console.log('Pressato');
+	}
+
 	render() {
 		return (
-			<View style={{ backgroundColor: 'white', flex: 1 }}>
+			<ScrollView style={{ backgroundColor: 'white', flex: 1 }}>
 				<Text>
 					<Translate id="welcome" />
 				</Text>
-				<CustomInput label="email" type="email" placeholder="inserisci email" />
-				<CustomInput label="password" type="password" placeholder="inserisci password" />
-			</View>
+				<CustomInput
+					label="email"
+					showLabel={false}
+					type="email"
+					placeholder="inserisci email"
+					onChangeField={this.onChangeField}
+				/>
+				<CustomInput
+					label="password"
+					type="password"
+					placeholder="inserisci password"
+					onChangeField={this.onChangeField}
+				/>
+				<CustomInput label="name" type="text" placeholder="inserisci nome" onChangeField={this.onChangeField} />
+				<CustomInput
+					label="phone"
+					type="phone"
+					placeholder="inserisci telefono"
+					onChangeField={this.onChangeField}
+				/>
+
+				<CustomButton text="bottone" onPress={() => this.onPress()} />
+			</ScrollView>
 		);
 	}
 }
