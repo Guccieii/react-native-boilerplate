a749a474aee1fdbe8011cf57265edf5500483bd3
Vincenzo Guccione <v.guccione@6mobile.mobi>
2020-07-16 12:47:06 +0200

    Added vector-icons, underscore and moment

--- src/components/CustomButton/index.js
+++ src/components/CustomButton/index.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-export default class CustomInput extends Component {
-	render() {
-		const { onPress, text } = this.props;
-		return (
-			<TouchableOpacity
-				onPress={onPress}
-				style={{
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-                    backgroundColor: 'red',
-                    borderWidth: 2,
-                    borderColor: 'red',
-					marginHorizontal: 16,
-					height: 48,
-					borderRadius: 16,
-					marginTop: 16
-				}}
-			>
-				<Text style={{ fontSize: 16 }}>{text}</Text>
-			</TouchableOpacity>
-		);
-	}
-}
+const CustomButton = (props) => {
+	const { onPress, text } = props;
+	return (
+		<TouchableOpacity
+			onPress={onPress}
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				backgroundColor: 'red',
+				borderWidth: 2,
+				borderColor: 'red',
+				marginHorizontal: 16,
+				height: 48,
+				borderRadius: 16,
+				marginTop: 16
+			}}
+		>
+			<Text style={{ fontSize: 16 }}>{text}</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default CustomButton;
